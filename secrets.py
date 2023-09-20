22a33753e6f353200a2a534a4d31e61e060283d3
Rishikesh.Shendkar <rishikesh.shendkar@cyberproof.com>
2023-09-20 18:40:01 +0530

    gitignore update

--- secrets.py
+++
@@ -1,8 +0,0 @@
-# Telegram bot API token
-TELEGRAM_BOT_TOKEN = 'YOUR_TOKEN_ID'
-# Your chat ID, you can find this out by messaging the bot and checking the updates
-CHAT_ID = 'YOUR_CHAT_ID'
-# Cost of your internet plan per month (Rupees)
-INTERNET_COST = 999
-# Your internet plan speed (Mbps)
-INTERNET_SPEED = 100
