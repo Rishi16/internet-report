1310288aa807fe6c6acc4550b571260573b1817e
Rishikesh.Shendkar <rishikesh.shendkar@cyberproof.com>
2023-09-20 15:03:31 +0530

    secrets added

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# Telegram bot API token
+TELEGRAM_BOT_TOKEN = 'YOUR_BOT_TOKEN'
+# Your chat ID, you can find this out by messaging the bot and checking the updates
+CHAT_ID = 'YOUR_CHAT_ID'
+# Cost of your internet plan per month (Rupees)
+INTERNET_COST = 999
+# Your internet plan speed (Mbps)
+INTERNET_SPEED = 100
